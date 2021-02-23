5c0989b67f3ef7ad138f95c4876bccef66d1705d
Steven Murray <tstevenm@gmail.com>
2021-02-23 06:45:50 -0600

    Database update - add ProductCategory entity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,6 @@ namespace API.Data
         public DbSet<AppUser> Users {get; set;}
         public DbSet<Gender> Genders {get; set;}
         public DbSet<Age> Ages {get; set;}
+        public DbSet<ProductCategory> ProductCategories {get; set;}
     }
 }
---
+++ API/Entities/ProductCategory.cs
@@ -0,0 +1,8 @@
+namespace API.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+    }
+}
